19274332570e6130d1131322d84067dda7593919
Aurelija Petrauskaite <aurelija35@gmail.com>
2023-01-05 22:29:37 +0200

    Vaziuoja tankas tusciam lauke.

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ ikona = pygame.image.load("tankiukas1.png")
 pygame.display.set_icon(ikona)  # kad pakeisti ikonele i tanka
 
 clock = pygame.time.Clock()
-tanko_figura = pygame.image.load("kitoks_tankas.png")
+tanko_figura = pygame.image.load("mini_tankas.png")
 
 
 def tankas(x, y):
@@ -22,48 +22,37 @@ x = 50
 y = 50
 plotis = 80
 aukstis = 40
-tempas = 1
-x_juda = 0
-y_juda = 0
+tempas = 5
+
 
 # tanko_figura = pygame.image.load("kitoks_tankas.png")
 # tanko_figura.get_rect(x, y )
 
 veikia = True
 while veikia:  # main game loop
+    visas_langas.fill((255, 255, 255))
     for event in pygame.event.get():
         if event.type == QUIT:  # tikrina ar veiksmas yra iseiti
             veikia = False
 
-    # kas_paspausta = pygame.key.get_pressed()
-    # if kas_paspausta[pygame.K_LEFT] and x > tempas: # Vakarai/ ir apriboju kad negaletu iseiti uz ribu
-    #     x -= tempas                  # kad judeti x asimi i kaire, turiu atimti
-    # if kas_paspausta[pygame.K_RIGHT] and x < 600 - plotis: # Rytai/ atimu is ekrano plocio savo ploti, kad neiseitu uz ribu i desine
-    #     x += tempas
-    # if kas_paspausta[pygame.K_UP] and y > tempas: # Siaure
-    #     y -= tempas
-    # if kas_paspausta[pygame.K_DOWN] and y < 600 - aukstis - tempas: # Pietus
-    #     y += tempas
-
     kas_paspausta = pygame.key.get_pressed()
-    if kas_paspausta[pygame.K_LEFT]:
-        x_juda = -1
-    elif kas_paspausta[pygame.K_RIGHT]:
-        x_juda = 1
-    elif kas_paspausta[pygame.K_UP]:
-        y_juda = 1
-    elif kas_paspausta[pygame.K_DOWN]:
-        y_juda = -1
+    if kas_paspausta[pygame.K_LEFT] and x > tempas: # Vakarai/ ir apriboju kad negaletu iseiti uz ribu
+        x -= tempas                  # kad judeti x asimi i kaire, turiu atimti
+    if kas_paspausta[pygame.K_RIGHT] and x < 600 - plotis: # Rytai/ atimu is ekrano plocio savo ploti, kad neiseitu uz ribu i desine
+        x += tempas
+    if kas_paspausta[pygame.K_UP] and y > tempas: # Siaure
+        y -= tempas
+    if kas_paspausta[pygame.K_DOWN] and y < 600 - aukstis - tempas: # Pietus
+        y += tempas
 
     # mini_tankas = pygame.transform.scale(tanko_figura, (x, y))
     # visas_langas.blit(mini_tankas, (80, 80))
 
     # visas_langas.blit(mini_tankas, (mini_tankas.x, mini_tankas.y))
     # pygame.draw.rect(visas_langas, (0, 255, 155), (x, y, plotis, aukstis)) #piesiu tanko figura, staciakampi
-    x += x_juda
-    y += y_juda
 
-    visas_langas.fill(0, 0, 0)
+
+    #visas_langas.fill(0, 0, 0)
     tankas(x, y)
 
     pygame.display.update()
